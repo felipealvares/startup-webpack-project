7e1a9005c46be5272064dc8fe0fb1fd89ed8f37f
Felipe Alvares <felipec.alvares@gmail.com>
2019-04-03 12:41:20 -0300

    Init Project

---
+++ source/index.js
@@ -0,0 +1,10 @@
+// SASS Files
+import './scss/default/imports.scss'
+
+// Scripts Dependencies
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+//import './js/core/includes'
+//import './js/plugins/cityButtons'
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const developmentMode = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const copyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: developmentMode ? 'development' : 'production',
+    entry: './source/index.js',
+    devServer: {
+        contentBase: './public',
+        port: 8080,
+    },
+    optimization: {
+        minimizer: [
+            new uglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new optimizeCssAssetsPlugin({})
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: __dirname + '/distribution'
+    },
+    plugins: [
+        new miniCssExtractPlugin({ filename: 'styles.css' }),
+        new copyWebpackPlugin([
+            { context: 'source/', from: '**/*.html' },
+            { context: 'source/', from: 'imgs/**/*' }
+        ])
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                miniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        }, {
+            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+            use: ['file-loader']
+        }]
+    }
+}
